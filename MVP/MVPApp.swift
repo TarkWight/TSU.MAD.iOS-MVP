0cd5a4293dcf8671ca213a7a961b0573b52bc998
Arsentiy Gonda <arsentiy.gonda@gmail.com>
2024-03-21 12:31:15 +0700

    Initial empty project

---
+++ MVP/MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPApp.swift
+//  MVP
+//
+//  Created by HITSStudent on 21.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
